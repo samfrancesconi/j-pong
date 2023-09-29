f24bf86bc2f2650aaf5d6c7cfddbc895ced09030
Samuele Francesconi <sam@MacBook-Pro-di-Samuele.local>
2023-09-29 16:04:07 +0200

    pio6

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <h1 class="text-center">THE SANDBOX</h1>
     <div class="flex flex-col items-center">
-        <img class="w-1/2 rounded-full" src="images/sand.png">
+        <img class="w-1/2 rounded-full" src="/images/sand.png">
         <br>
         <button class="bg-[#F7DEA4] p-2" @click="goTo()">Pio</button>
     </div>
